6dadf3535f3bd43a36385887649f8f6bf51f0b7e
CristianoSouza <cristianoantonio.souza10@gmail.com>
2019-05-07 11:23:03 -0300

    comparacao funcionando

--- compare-withCSV.py
+++ compare-withCSV.py
@@ -12,86 +12,28 @@ from re import search
 from re import findall
 from sys import stdin
 from string import capwords
+import csv
 
-entries = []
-entry = {}
-keys= { "title",
-        "journal", 
-        "year",
-        #"doi",
-        #"author",
-        "abstract",
-        "url"
-        #...
-}
+file_name_current_search = "artigos.csv"
+file_name_data_base = "artigos_base.csv"
+file_current_search = open(file_name_current_search)
+file_data_base = open(file_name_data_base)
 
-for line in stdin:
-    #print(entry)
-    if (match('^@', line.strip())):
-        #print(line.strip())
-        if entry != {}:
-            entries.append(entry)
-            entry = {}
-    elif (search('=', line.strip())):
-        key, value = [v.strip(" {},\n") for v in line.split("=")]
-        entry[key] = value
-        #print(value)
+file_result = open("result.csv", "wb")
+writer = csv.writer(file_result)
 
-entries.append(entry)    
+#lines = csv.reader(file)
+artigos_current_search = csv.DictReader(file_current_search)
 
-for item in entries:
-    #print(entries)
-    author = "Anonymous"
-    if "author" in item:
-        author = item["author"]
-    elif "authors" in item:
-        author = item["authors"]
-    elif "editor" in item:
-        author = item["editor"]
-    item["author"] = author
+for artigo_current_search in artigos_current_search:
+    print("1 - Nome:", artigo_current_search["Document Title"])
 
-    publish = "No publishing information"
-    if "journal" in item:
-        publish = item["journal"]
-    if "journaltitle" in item:
-        publish = item["journaltitle"]
-    elif "booktitle" in item:
-        publish = item["booktitle"]
-    elif "howpublished" in item:
-        publish = item["howpublished"]
-    elif "type" in item:
-        publish = item["type"]
-    elif "url" in item:
-        publish = "Website: {}".format(item["url"])
-    elif "crossref" in item:
-        publish = item["crossref"].replace("_", " ")
-        publish = capwords(publish)
-    elif "publisher" in item:
-        publish = item["publisher"]
-    item["publish"] = publish
-    
-    year = "Unknown year"
-    if "year" in item:
-        year = item["year"]
-    item["year"] = year
+    file_data_base = open(file_name_data_base)
+    artigos_data_base = csv.DictReader(file_data_base)
+    for artigo_data_base in artigos_data_base:
+        print("2 - Nome:", artigo_data_base["Document Title"])
 
-    title = "Unknown title"
-    if "title" in item:
-        title = item["title"]
-    item["title"] = title
-
-    abstract = "Unknown abstract"
-    if "abstract" in item:
-        abstract = item["abstract"]
-    item["abstract"] = abstract
-
-    journal = "Unknown journal"
-    if "journal" in item:
-        journal = item["journal"]
-    item["journal"] = journal
-
-    string = ""
-    for k in keys:
-        string += item[k] + "; "
-        
-    print(string)
+        if (artigo_current_search["Document Title"] == artigo_data_base["Document Title"]):  
+            print("Combinou")
+            writer.writerow([artigo_current_search["Document Title"], artigo_current_search["Authors"], artigo_current_search["Publication_Year"]])
+            break
